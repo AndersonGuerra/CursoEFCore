2842496084d0ad6cc7ca254fc9e09d90bd9e1403
Anderson Guerra <and.guerra@outlook.com>
2022-08-05 15:00:04 -0300

    Commit inicial

---
+++ Curso/Program.cs
@@ -0,0 +1,26 @@
+﻿using CursoEFCore.Data;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+Console.WriteLine("Hello, World!");
+
+static void InserirDados()
+{
+    var produto = new Produto
+    {
+        Descricao = "Produto Teste",
+        CodigoBarras = "12341232132132",
+        Valor = 10m,
+        TipoProduto = TipoProduto.MercadoriaParaRevenda,
+        Ativo = true
+    };
+
+    using var db = new ApplicationContext();
+    db.Database.Migrate();
+    db.Produtos.Add(produto);
+    var registros = db.SaveChanges();
+    Console.WriteLine($"Total de registro(s): {registros}");
+
+}
+
+InserirDados();
